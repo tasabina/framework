070f5368837c06868431d7583032c12e519adc45
taSabina <tasabina@yandex.ru>
2018-03-14 22:51:26 +1300

    first_week

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    $query = $_SERVER['QUERY_STRING'];
+
+    require '../vendor/core/Router.php';
+    require '../vendor/libs/function.php';
+
+//    Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+//    Router::add('posts/', ['controller' => 'Posts', 'action' => 'index']);
+//    Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+    Router::add('^$',['controller' => 'Main', 'action' => 'index'] );
+    Router::add('([a-z-]+)/([a-z-]+)');
+
+    debug(Router::getRoutes());
+
+    if(Router::matchRoute($query)){
+        debug(Router::getRoute());
+    }else{
+        echo '404';
+    }
